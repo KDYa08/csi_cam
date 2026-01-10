f75a5a97fc000c704504304f0d3f22ea0f7e59c5
KDYa08 <owen0821@naver.com>
2026-01-10 13:44:06 +0900

    csi_camera pkg upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'csi_cam'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kdya08',
+    maintainer_email='kdya08@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'cam1= csi_cam.csi_cam_1:main',
+            'cam2= csi_cam.csi_cam_2:main'
+        ],
+    },
+)
